442c8b4c710ef9fa77dd2c22810fbe73c96a5ced
Anthony Gomez <Agomez@stagewood.com>
2023-10-13 11:50:29 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace OldStyleApp
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string myStr = "backslash \n\"hello world\" ";
+                    Console.WriteLine(myStr);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
